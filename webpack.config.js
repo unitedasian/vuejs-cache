9bd9a0b7b364fce07496efea6261a54a3f0d3ff1
sabir <manandharsabir@hotmail.com>
2018-04-19 11:14:36 +0545

    changed entry point

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ const path = require('path');
 module.exports = merge(require('./webpack.base'), {
   context: __dirname,
 
-  entry: ['./src/index.js'],
+  entry: './src/index.js',
 
   output: {
     path: path.resolve(__dirname, 'dist'),
